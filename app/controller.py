ab1b8400329fc4138869895b57c2b5aa8b20f360
CatAnderson <thecatanderson100@gmail.com>
2020-09-26 20:06:49 +0100

    integrated app.route with html, finished MVP

--- app/controller.py
+++ app/controller.py
@@ -1,24 +1,36 @@
 from app import app
 from flask import render_template
+from app.models.game import *
+from app.models.player import *
 
 
-@app.route('/<player1>/<player2>')
-def index(player1, player2):
-    if player1 == "rock" and player2 == "scissors":
-        return "player1 wins!!"
-    elif player1 == "rock" and player2 == "paper":
-        return "player2 wins!!"
-    elif player1 == "rock" and player2 == "rock":
-        return "draw!!"
-    elif player1 == "scissors" and player2 == "rock":
-        return "player2 wins!!"
-    elif player1 == "scissors" and player2 == "paper":
-        return "player1 wins!!"
-    elif player1 == "scissors" and player2 == "scissors":
-        return "draw!!"
-    elif player1 == "paper" and player2 == "rock":
-        return "player1 wins!!"
-    elif player1 == "paper" and player2 == "scissors":
-        return "player2 wins!!"
-    elif player1 == "paper" and player2 == "paper":
-        return "draw!!"
+# @app.route('/<player1>/<player2>')
+# def index(player1, player2):
+#     if player1 == "rock" and player2 == "scissors":
+#         return "player1 wins!!"
+#     elif player1 == "rock" and player2 == "paper":
+#         return "player2 wins!!"
+#     elif player1 == "rock" and player2 == "rock":
+#         return "draw!!"
+#     elif player1 == "scissors" and player2 == "rock":
+#         return "player2 wins!!"
+#     elif player1 == "scissors" and player2 == "paper":
+#         return "player1 wins!!"
+#     elif player1 == "scissors" and player2 == "scissors":
+#         return "draw!!"
+#     elif player1 == "paper" and player2 == "rock":
+#         return "player1 wins!!"
+#     elif player1 == "paper" and player2 == "scissors":
+#         return "player2 wins!!"
+#     elif player1 == "paper" and player2 == "paper":
+#         return "draw!!"
+
+@app.route('/')
+def index():
+    player_rock = Player("Hayley", "rock")
+    player_paper = Player("Aly", "paper")
+    player_scissors = Player("Mel", "scissors")
+
+    game1 = Game(player_rock, player_scissors)
+    result = game1.rps_game(player_rock.choice, player_scissors.choice)
+    return render_template("index.html", result=result)
