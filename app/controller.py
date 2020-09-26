da6fe0872f4b7367848d9359c22b4269ec99c83a
CatAnderson <thecatanderson100@gmail.com>
2020-09-26 10:52:24 +0100

    got game working from controller.py file

--- app/controller.py
+++ app/controller.py
@@ -2,6 +2,23 @@ from app import app
 from flask import render_template
 
 
-@app.route('/')
-def index():
-    return "HELLLOOOOOO"
+@app.route('/<player1>/<player2>')
+def index(player1, player2):
+    if player1 == "rock" and player2 == "scissors":
+        return "player1 wins!!"
+    elif player1 == "rock" and player2 == "paper":
+        return "player2 wins!!"
+    elif player1 == "rock" and player2 == "rock":
+        return "draw!!"
+    elif player1 == "scissors" and player2 == "rock":
+        return "player2 wins!!"
+    elif player1 == "scissors" and player2 == "paper":
+        return "player1 wins!!"
+    elif player1 == "scissors" and player2 == "scissors":
+        return "draw!!"
+    elif player1 == "paper" and player2 == "rock":
+        return "player1 wins!!"
+    elif player1 == "paper" and player2 == "scissors":
+        return "player2 wins!!"
+    elif player1 == "paper" and player2 == "paper":
+        return "draw!!"
